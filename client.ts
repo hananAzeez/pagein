b7b7d09c600c56628d8438ec21d718184ab91c9e
hananAzeez <hanan.azeez112@gmail.com>
2023-10-20 10:12:01 +0530

    sanity blog build error fix

--- client.ts
+++ client.ts
@@ -3,5 +3,6 @@ import sanityClient from '@sanity/client'
 export default sanityClient({
   projectId: 'a630cvdc', // you can find this in sanity.json
   dataset: 'works', // or the name you chose in step 1
+  apiVersion: '2021-08-31', // use a UTC date string
   useCdn: true // `false` if you want to ensure fresh data
 })
---
+++ pages/works/[slug].tsx
@@ -0,0 +1,140 @@
+/* eslint-disable @next/next/no-img-element */
+// [slug].tsx
+
+import groq from 'groq'
+import imageUrlBuilder from '@sanity/image-url'
+import {PortableText} from '@portabletext/react'
+import client from '../../client'
+import styles from './work.module.css'
+import Footer from '../../components/footer'
+import Navbar from '../../components/navbar'
+import { SanityImageSource } from '@sanity/image-url/lib/types/types'
+import { ReactNode } from 'react';
+
+function urlFor (source: SanityImageSource) {
+  return imageUrlBuilder(client).image(source)
+}
+
+interface ImageComponentProps {
+  value: {
+    _type: string;
+    _key: string;
+    asset: {
+      _ref: string;
+      _type: string;
+    };
+    alt?: string;
+  };
+}
+
+
+const ptComponents = {
+  types: {
+    image: ({ value }: ImageComponentProps): ReactNode => {
+      if (!value?.asset?._ref) {
+        return null;
+      }
+
+      const imageUrl = urlFor(value).auto('format').url(); // Extract the URL
+      return (
+        <img className={styles.images}
+          alt={value.alt || ' '}
+          loading="lazy"
+          src={imageUrl}
+        />
+      );
+    }
+  }
+}
+
+interface Post {
+  title?: string;
+  name?: string;
+  categories: string[] | null;
+  authorImage?: {
+    _type: string;
+    asset: {
+      _ref: string;
+    };
+  };
+  body: Array<{ _type: string; _key: string; asset?: { _type: string; _ref: string } } | { children: Array<{ _type: string; text: string }>; _type: string; style: string; _key: string; markDefs: any[] }>;
+}
+
+const Post = ({ post }: { post: Post }) => {
+  console.log(post)
+  if (!post) {
+    // Handle the case where 'post' is undefined
+    return <div>Loading...</div>;
+  }
+
+  const {
+    title = 'Missing title',
+    name = 'Missing name',
+    categories,
+    authorImage,
+    body = []
+  } = post;
+
+  return (
+    <main className='px-5 mx-auto lg:px-28 xl:px-0 py-8 overflow-hidden'>
+      <div className="max-w-7xl mx-auto">
+        <Navbar color='black' />
+        <article className={styles.article}>
+          <h1 className={`${styles.title}`}>{title}</h1>
+          {categories && (
+            <ul>
+              Posted in
+              {categories.map(category => <li key={category}>{category}</li>)}
+            </ul>
+          )}
+          {authorImage && (
+            <div>
+              <img
+                src={urlFor(authorImage)
+                  .width(50)
+                  .url()}
+                alt={`${name}'s picture`}
+              />
+            </div>
+          )}
+          <PortableText
+            value={body}
+            components={ptComponents}
+          />
+        </article>
+      </div>
+      <Footer />
+    </main>
+  );
+};
+
+
+const query = groq`*[_type == "post" && slug.current == $slug][0]{
+  title,
+  "name": author->name,
+  "categories": categories[]->title,
+  "authorImage": author->image,
+  body
+}`
+export async function getStaticPaths() {
+  const paths = await client.fetch(
+    groq`*[_type == "post" && defined(slug.current)][].slug.current`
+  )
+
+  return {
+    paths: paths.map((slug: any) => ({params: {slug}})),
+    fallback: true,
+  }
+}
+
+export async function getStaticProps(context: { params: { slug?: "" | undefined } }) {
+  // It's important to default the slug so that it doesn't return "undefined"
+  const { slug = "" } = context.params
+  const post = await client.fetch(query, { slug })
+  return {
+    props: {
+      post
+    }
+  }
+}
+export default Post
