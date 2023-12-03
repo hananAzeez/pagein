d27529d8ec1e0b1b7ee780bae9478ffd5872a41f
hananAzeez <hanan.azeez112@gmail.com>
2023-12-03 18:18:10 +0530

    blog page design

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -39,39 +39,42 @@ const Works = ({posts}: Props) => {
 
           <section className="works panel bg-offWhite">
             <div className="max-w-6xl 2xl:max-w-7xl mx-auto py-20 px-5 xl:px-0">
-              <div className="flex flex-col xl:flex-row items-center justify-between text-offBlack">
+              {/* <div className="flex flex-col xl:flex-row items-center justify-between text-offBlack">
                 <AnimatedElement>
                 <h2 className=" font-bold text-4xl leading-[43px] lg:text-[64px] lg:leading-[80px] !text-offBlack w-full max-w-4xl tracking-tight revealType">
                   You might love what we built to our clients
                 </h2>
                 </AnimatedElement>
-              </div>
+              </div> */}
 
               <div className="grid xl:grid-cols-2 gap-8 lg:gap-x-8 lg:gap-y-10 mt-8 md:mt-10">
                 {posts.map((work,index) => (
                   <AnimatedElement key={work._id} delay={(index+1) % 2 === 0 ? 0.1 : 0}>
                     <ClientSideRoute route={`/works/${work.slug.current}`}>
                   <div className="work1 cursor-pointer "  >
+                    <div className="relative">
                     <img
                       src={urlFor(work.mainImage).auto('format').url()}
                       alt="works"
                       className="rounded-xl lg:rounded-2xl"
                     />
-                    <div className="mt-3 md:mt-6">
-                      <h6 className="text-2xl xl:text-4xl font-semibold revealType">
-                        {work.title}
-                      </h6>
-
-                      <div className="mt-3 flex gap-2 flex-wrap">
+                    <div className="mt-3 flex gap-2 flex-wrap absolute left-10 top-5">
                         {work.categories.map((tag) => (
                           <p
-                            className="text-sm md:text-base font-saira  py-2 px-8 rounded-full border border-offBlack revealType inline-block"
+                            className="text-sm md:text-base font-saira  py-2 px-8 rounded-full bg-primary border border-offBlack revealType inline-block"
                             key={tag._id}
                           >
                             {tag.title}
                           </p>
                         ))}
                       </div>
+                    </div>
+                    <div className="mt-3 md:mt-6">
+                      <h6 className="text-2xl xl:text-4xl font-semibold revealType">
+                        {work.title}
+                      </h6>
+
+                      
                     </div>
                   </div>
                   </ClientSideRoute>
